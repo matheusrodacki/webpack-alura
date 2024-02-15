91cf7894353fd273412b4858fef7f4c04b8dee9a
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-02-15 11:02:50 -0300

    minimiza arquivos em prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -13,6 +14,10 @@ module.exports = {
     rules: [
       { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
     ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerWebpackPlugin()],
   },
   plugins: [
     new HtmlWebpackPlugin({
