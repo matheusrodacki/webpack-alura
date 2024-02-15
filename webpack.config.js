00aecaee58cce3ee41ab6891e8befa6e37b042ce
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-02-15 10:52:04 -0300

    salva o modulo css como arquivo style.css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
@@ -8,12 +9,17 @@ module.exports = {
     path: path.resolve(__dirname, 'app/dist'),
     clean: true,
   },
-  module: { rules: [{ test: /\.css$/, use: ['style-loader', 'css-loader'] }] },
+  module: {
+    rules: [
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './app/src/app.html',
       filename: 'app.html',
       hash: true,
     }),
+    new MiniCssExtractPlugin({ filename: 'style.css' }),
   ],
 };
